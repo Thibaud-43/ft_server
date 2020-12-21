832f6bf6a6ec809a3541bdba0b99a78c9d143a01
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2020-12-21 18:04:22 +0100

    ok

--- srcs/initdb.sql
+++ srcs/initdb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-GRANT ALL ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'co241tii';
 
 update mysql.user set plugin='mysql_native_password' where user='root';
 
--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'wordpress');
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'co241tii');
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
